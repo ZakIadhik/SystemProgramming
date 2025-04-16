23a93e03942b7424ff8e67147436ce4705dfead0
ZakIadhik <nazarov.ramin.2004@mail.ru>
2025-04-16 22:21:05 +0400

    Add files via upload

---
+++ TaskAsync/AsyncStudentService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace EfThreadingDemo
+{
+    public class AsyncStudentService
+    {
+        public async Task AddStudentAsync(string name)
+        {
+            Console.WriteLine($"[Async Add] Thread: {Thread.CurrentThread.ManagedThreadId}");
+
+            using var context = new AppDbContext();
+            await context.Students.AddAsync(new Student { Name = name });
+            await context.SaveChangesAsync();
+
+            Console.WriteLine($"[Async Add -> DB] Thread: {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+        public async Task ShowAllStudentsAsync()
+        {
+            Console.WriteLine($"[Async Show] Thread: {Thread.CurrentThread.ManagedThreadId}");
+
+            using var context = new AppDbContext();
+            var students = await context.Students.ToListAsync();
+
+            Console.WriteLine($"[Async Show -> DB] Thread: {Thread.CurrentThread.ManagedThreadId}");
+            foreach (var student in students)
+                Console.WriteLine($"[Async] {student.Id}: {student.Name}");
+        }
+    }
+}
